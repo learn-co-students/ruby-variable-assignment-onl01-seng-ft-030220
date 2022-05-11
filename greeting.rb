b7c713bda5a39feb1978b96ac484e6b16963aa64
winifredf <winifred.frierson@gmail.com>
2022-05-11 10:09:19 -0400

    greetings params arguments samples

--- greeting.rb
+++ greeting.rb
@@ -8,3 +8,28 @@ greeting
 greeting
 greeting
 greeting
+
+
+def greeting(name)
+    puts "Hello, #{name}!"
+end
+
+greeting("Sophie")
+greeting("Ian")
+greeting("Winifred")
+greeting("Max")
+
+def greeting_a_person(name)
+    "Hello #{name}"
+end
+
+
+
+def greeting_programmer(name, language)
+    puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting_programmer("Amanda", "Python")
+greeting_programmer("Stu", "Ruby")
+greeting_programmer("Amanal", "Elixir")
+
